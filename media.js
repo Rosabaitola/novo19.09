3021860e8ac704a76a9f22a24eb0fd520f7af106
Rosa Graciosa <126572302+Rosabaitola@users.noreply.github.com>
2023-10-03 11:57:21 -0300

    03/10

--- media.js
+++ media.js
@@ -1,28 +1,28 @@
-const form = documment.querySelector("form")
-const resp1 = documment.querySelector("h3")
-const resp2 = documment.querySelector("h4")
+//cria referencia ao form e elementos de respostas do programa
 
-form.addEventlistener("Submit",(e)=>{
-    e.preventDefault()
+const form = document.querySelector("form")
+const resp1 = document.querySelector("h3")
+const resp2 = document.querySelector("h4")
 
-    const nome = form.inNome.value
-    const nota1 = Number(form.inNota1.value)
-    const nota2 = Number(form.inNota2.value)
-    const media = (nota1 + nota2)/2
-    resp1.innerText = `Média das Notas ${media.toFixed(2)}`
+//cria um ouvinte
 
-        if(media >= 7) {
-        resp2.innerText = `Parabéns ${nome}! Você foi 
-    aprovado(a)`
-        resp2.style.color = "blue"
-    }   else if (media >= 4) {
-        resp2.innerText = `Atenção ${nome}! Você está de 
-    exame`
-            resp2.style.color = "green"
-        }else{
-            resp2.innerText = `Ops ${nome}! Você foi 
-        reprovado(a)`
-            resp2.style.color = "red"
-        }
+form.addEventListener("submit" , (e) => {
+      e.preventDefault()    //evita o envio form
+const nome = form.inNome.value
+const nota1 = Number(form.inNota1.value)
+const nota2 = Number(form.inNota2.value)
+const media = (nota1 + nota2)
+resp1.innerText = `Medias das notas ${media.toFixed(2)}`
+//Criar as coondicionais
 
+if(media >= 7) {
+    resp2.innerText = `Parabens  ${nome}! Você foi aprovado`
+     resp2.style.color = "blue"
+    } else if (media >= 4 ) {
+        resp2.innerText = `Atenção ${nome}! você esta de exame`
+        resp2.style.color = "green"
+    }  else {
+        resp2.innerText =  `Ops ${nome}! você foi reprovado`
+        resp2.style.color = "red"
+    }
 })
